2666a15a8d552b18f18b20498bf0214a25eb48f1
Roman <shelepov-rmn@mail.ru>
2024-12-06 21:20:08 +0300

    Add: Data info

--- cypress/e2e/pokemonbattle.cy.js
+++ cypress/e2e/pokemonbattle.cy.js
@@ -1,34 +1,25 @@
-const USER_LOGIN = "";
-const USER_PASSWORD = "";
+import * as data from "../helpers/default_data.json";
 
 describe("e2e", function () {
-    it("Покупка аватара для тренера", function () {
-        cy.visit("https://pokemonbattle.ru/login"); // Зайти на сайт Pokemonbattle
-        cy.get(":nth-child(1) > .auth__input").type(USER_LOGIN);
-        cy.get("#password").type(USER_PASSWORD); // Ввести свой пароль
-        cy.get(".auth__button").click(); // Нажать кнопку "Войти"
-        cy.wait(1500);
+  it("Покупка аватара для тренера", function () {
+    cy.visit("https://pokemonbattle.ru/login"); // Зайти на сайт Pokemonbattle
+    cy.get(":nth-child(1) > .auth__input").type(data.login);
+    cy.get("#password").type(data.password); // Ввести свой пароль
+    cy.get(".auth__button").click(); // Нажать кнопку "Войти"
+    cy.wait(1500);
 
-        cy.get(".header__container > .header__id").click(); // Открыть личный каинет
-        cy.get('[href="/shop"]').click(); // Открыть страницу покупки аватара
-        cy.get(".available > button").first().click(); // Нажать "Купить" у первого доступного к покупке аватара
-        cy.wait(1500);
+    cy.get(".header__container > .header__id").click(); // Открыть личный каинет
+    cy.get('[href="/shop"]').click(); // Открыть страницу покупки аватара
+    cy.get(".available > button").first().click(); // Нажать "Купить" у первого доступного к покупке аватара
+    cy.wait(1500);
 
-        cy.get(".pay__payform-v2 > :nth-child(2) > .pay_base-input-v2").type(
-            "4111111111111111"
-        ); // Ввести номер карты
-        cy.get(":nth-child(1) > .pay_base-input-v2").type("1225"); // Ввести срок карты
-        cy.get(".pay-inputs-box > :nth-child(2) > .pay_base-input-v2").type(
-            "125"
-        ); // Ввести CVV
-        cy.get(".pay__input-box-last-of > .pay_base-input-v2").type(
-            "ANTON LOGVINOV"
-        ); // Ввести владельца карты
-        cy.get(".pay-btn").click(); // Нажать "Оплатить"
-        cy.get("#cardnumber").type("56456"); // Ввести код проверки из пуша
-        cy.get(".payment__submit-button").click(); // Нажать "Подтвердить"
-        cy.get(".payment__success1")
-            .should("be.visible")
-            .contains("Покупка прошла успешно");
-    }); // Проверка на наличие и видимость сообщения "Покупка прошла успешно"
+    cy.get(".pay__payform-v2 > :nth-child(2) > .pay_base-input-v2").type("4111111111111111"); // Ввести номер карты
+    cy.get(":nth-child(1) > .pay_base-input-v2").type("1225"); // Ввести срок карты
+    cy.get(".pay-inputs-box > :nth-child(2) > .pay_base-input-v2").type("125"); // Ввести CVV
+    cy.get(".pay__input-box-last-of > .pay_base-input-v2").type("ANTON LOGVINOV"); // Ввести владельца карты
+    cy.get(".pay-btn").click(); // Нажать "Оплатить"
+    cy.get("#cardnumber").type("56456"); // Ввести код проверки из пуша
+    cy.get(".payment__submit-button").click(); // Нажать "Подтвердить"
+    cy.get(".payment__success1").should("be.visible").contains("Покупка прошла успешно");
+  }); // Проверка на наличие и видимость сообщения "Покупка прошла успешно"
 });
